2576a58e41811722af301d0d193ca1c99e47af24
Sergey Bolotnikov <sbolotnikov@gmail.com>
2022-01-18 02:39:32 -0500

    fixing api/contact

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,50 +1,40 @@
-export default function (req, res) {
+import nc from 'next-connect';
+import { onError } from '../../utils/error';
+const handler = nc({
+  onError,
+});
+handler.post(async(req, res) =>{
     // need query db to get mainEmail
 
 
     const nodemailer = require('nodemailer');
-    const { google } = require('googleapis');
-    
-    const oAuth2Client = new google.auth.OAuth2(
-      process.env.CLIENT_ID_MAIL,
-      process.env.CLIENT_SECRET_MAIL,
-      process.env.REDIRECT_URI
-    );
-    oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
-    
+
     async function sendMail() {
-      try {
-        const accessToken = await oAuth2Client.getAccessToken();
-    
-        const transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-            type: 'OAuth2',
-            user: process.env.EMAIL_SERVER_USER,
-            clientId: process.env.CLIENT_ID_MAIL,
-            clientSecret: process.env.CLIENT_SECRET_MAIL,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: accessToken,
-          },
-        });
-        const mailData = {
-          from: req.body.email,
-          to: req.body.mainEmail,
-          subject: `Message From ${req.body.name}`,
-          text: req.body.message + " | Sent from: " + req.body.email,
-          html: `<div>${req.body.message}</div><p>Sent from:
-          ${req.body.email}</p>`
+        let transporter = nodemailer.createTransport(
+      {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL_SERVER_USER, // generated ethereal user
+          pass: process.env.EMAIL_SERVER_PASSWORD // generated ethereal password
         }
- 
-        const result = await transporter.sendMail(mailData);
-        return result;
-      } catch (error) {
-        return error;
       }
+      );
+      const result = await transporter.sendMail({
+        from: req.body.email,
+        to: req.body.mainEmail,
+        subject: `Message From ${req.body.name}`,
+        text: req.body.message + " | Sent from: " + req.body.email,
+        html: `<div>${req.body.message}</div><p>Sent from:
+        ${req.body.email}</p>`,
+    });
+        console.log("result: ",result)
+        res.status(200).send('Email sent...', result)
+        return 
+
     }
     
     sendMail()
-      .then((result) => res.status(200).send('Email sent...', result))
-      .catch((error) => res.status(500).send(error.message));
-
-  }
+  });
+  export default handler;
