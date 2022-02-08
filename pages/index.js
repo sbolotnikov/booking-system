99154e41d5bc75ac7294ba23159d71378ed1abdd
Sergey Bolotnikov <sbolotnikov@gmail.com>
2022-02-08 12:13:57 -0500

    sign anim fix

--- pages/index.js
+++ pages/index.js
@@ -34,60 +34,53 @@ export default function Home() {
       setScrollTop(currentPosition <= 0 ? 0 : currentPosition);
     }
 
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
   }, [scrollTop]);
   // useEffect(() => {
   //   window.addEventListener("onscroll", (e) => setScrollDown(true));
   // }, []);
-//   useEffect(() => {
-//     const onScroll = () => {if (window.pageYOffset>0) setScrollDown(true)}
-//     // clean up code
-//     window.removeEventListener('scroll', onScroll);
-//     window.addEventListener('scroll', onScroll, { passive: true });
-//     return () => window.removeEventListener('scroll', onScroll);
-// }, []);
+  //   useEffect(() => {
+  //     const onScroll = () => {if (window.pageYOffset>0) setScrollDown(true)}
+  //     // clean up code
+  //     window.removeEventListener('scroll', onScroll);
+  //     window.addEventListener('scroll', onScroll, { passive: true });
+  //     return () => window.removeEventListener('scroll', onScroll);
+  // }, []);
   return (
     <>
       <Head></Head>
       <div className="w-full flex flex-col justify-center  items-center">
         <div className="w-full grid xs:grid-cols-1 sm:grid-cols-1 phone:grid-cols-2 max-w-[1170px]">
-          <div className="relative w-80 ">
-<div className="absolute  top-[5rem] right-[4.5rem]">            
-           <div className="flame"></div>
-</div>
+          <div className="relative w-80 h-full ">
+            <div className="absolute  top-[5rem] right-[4.5rem]">
+              <div className="flame"></div>
+            </div>
             <img
               className="w-64 "
               style={{ filter: 'drop-shadow(10px 5px 4px #7D8800)' }} //#4444dd  shadow change
               src={'/icons/svg/light.svg'}
               alt="hero"
             />
+              {/* className="animate_pendulum absolute w-60 max-w-[300px]  -bottom-10 right-12 origin-top-center rotate-12" */}
+
             <img
-              className="animate_pendulum absolute w-60 max-w-[300px]  -bottom-10 right-12 origin-top-center rotate-12"
+              className="animate_pendulum absolute w-60 max-w-[300px]  top-[18.32rem] left-[2.85rem] origin-top-center rotate-12"
               style={{ filter: 'drop-shadow(10px 5px 4px #7D8800)' }} //#4444dd  shadow change
               src={'/icons/svg/sign.svg'}
               alt="hero"
             />
-
           </div>
           <div className="w-full inner-wrap_front flex  flex-col justify-center  items-center">
             <h2 className="text-4xl mt-6">Таинственный переулок</h2>
             <h3 className="font-bold text-2xl mt-5">
               центр квестов и пряток в{' '}
               <Link href="/contacts/0">
-                <div
-                  className="text-[#7D8800] cursor-pointer"
-                >
-                  Челябинске.
-                </div>
+                <div className="text-[#7D8800] cursor-pointer">Челябинске.</div>
               </Link>
             </h3>
-            <p className="font-bold mb-2">
-              квесты в реальности 
-            </p>
-            <p className="font-bold mb-2">
-            прятки во тьме
-            </p>
+            <p className="font-bold mb-2">квесты в реальности</p>
+            <p className="font-bold mb-2">прятки во тьме</p>
             <button
               className="btnBlue"
               onClick={(e) => {
@@ -110,7 +103,6 @@ export default function Home() {
 
         <img className="w-[75vw] h-[65vh]" style={{filter: 'drop-shadow(10px 5px 4px #4444dd)'}} src={"/icons/houses_cloud.svg"}  alt="hero"  />
       </div> */}
-      
     </>
   );
 }
