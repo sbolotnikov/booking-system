dbe671f42fd204610a009e99f7b8add43335ad57
Sergey Bolotnikov <sbolotnikov@gmail.com>
2022-06-18 16:09:40 -0400

    change to sendgrid

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,40 +1,40 @@
-// const sgMail = require('@sendgrid/mail')
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail')
+// const nodemailer = require('nodemailer');
 export const sendEmail=async(mailData) =>{
   
-  // sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-  // sgMail
-  //   .send(mailData)
-  //   .then(() => {
-  //     console.log('Email sent')
-  //   })
-  //   .catch((error) => {
-  //     console.error(error)
-  //   })
+  sgMail
+    .send(mailData)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 
-        try {
+//         try {
     
-        const transporter = nodemailer.createTransport(
+//         const transporter = nodemailer.createTransport(
 
-        {
-          host: process.env.EMAIL_SERVER_HOST,
-          // port: process.env.EMAIL_SERVER_PORT,
-          port: 587,
-          secure: false,
+//         {
+//           host: process.env.EMAIL_SERVER_HOST,
+//           // port: process.env.EMAIL_SERVER_PORT,
+//           port: 587,
+//           secure: false,
 
-          // secure: true,
-          auth: {
-            user: process.env.EMAIL_SERVER_USER, 
-            pass: process.env.EMAIL_SERVER_PASSWORD, 
-          },
-        }
-        );
+//           // secure: true,
+//           auth: {
+//             user: process.env.EMAIL_SERVER_USER, 
+//             pass: process.env.EMAIL_SERVER_PASSWORD, 
+//           },
+//         }
+//         );
 
-    const result = await transporter.sendMail(mailData);
-    return result;
-} catch (error) {
-  return error;
-}
+//     const result = await transporter.sendMail(mailData);
+//     return result;
+// } catch (error) {
+//   return error;
+// }
   }
   
