4298580846186491a279caf9448a9e25cff9588e
Sergey Bolotnikov <sbolotnikov@gmail.com>
2022-07-02 12:55:54 -0400

    question answers fix

--- components/accordionFAQ.js
+++ components/accordionFAQ.js
@@ -1,6 +1,8 @@
 import { useState } from "react"
 function AccordionFAQ(props) {
-
+    const callPictureView=({link,title})=>{
+       console.log(link, title)
+    }
     const [visible, setVisible] = useState(-1);
     function handleVisible(e) {
         let n = parseInt(e.currentTarget.id);
@@ -16,19 +18,19 @@ function AccordionFAQ(props) {
         n!==visible?setVisible(n):setVisible(-1);
       }
     return (
-        <div className='relative m-8'>
+        <div className='relative m-8  flex flex-col justify-center items-center'>
 
         {props.options.map((item, j) => {
           return (
-            <div className="m-3" key={"option" + j} id={j+'.links'} onClick={e => handleVisible(e)} >
-              <div key={"question" + j} className="w-full flex">
+            <div className="m-3  flex flex-col justify-center items-center" key={"option" + j} id={j+'.links'} onClick={e => handleVisible(e)} >
+              <div key={"question" + j} className="flex">
                   <img className="w-5 h-5" src={"/icons/caret.svg"}  alt="open" id={"img."+j} key={"btnflip" + j}  />
                 
                 <h3 id={"question"+j} className="cursor-pointer text-left ml-4">{item.question}</h3>
                 
               </div>
              
-              {(j===visible) &&<p key={"answer" + j} style={{ cursor: "pointer" }}
+              {(j===visible) &&<p key={"answer" + j} onClick={e =>{e.stopPropagation();}} className="max-w-xl text-center"
                     dangerouslySetInnerHTML={{ __html:item.answer}}/>
               }
             </div>
