a110c40a6ef176d2296cbf707e6bb18579b6ee36
Sergey Bolotnikov <sbolotnikov@gmail.com>
2022-07-02 13:44:09 -0400

    change booking card for laptop

--- components/bookingCard.js
+++ components/bookingCard.js
@@ -29,7 +29,7 @@ function BookingCard(props) {
       <div className="inner-wrap flex flex-col justify-center items-center">
         <img className="w-10 h-10 mb-2 " src={props.item.img} alt="" />
         <h2 className="flex justify-center items-center">{props.item.name}</h2>
-        <h3 className="flex justify-center items-center h-64">{props.item.short}</h3>
+        <h3 className="flex justify-center items-center h-64 laptop:h-48">{props.item.short}</h3>
         <div className="m-3 relative" id={1+'.'+props.id+'.links'} onClick={e => handleVisible(e)} >
               <div  className="w-full flex">            
                 <div className="cursor-pointer text-left ml-4">КОМУ ПОДОЙДЕТ?</div>
